6620ed4ddc4771e98ca03ad152e1f7d7ebcc6fc1
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-04-30 14:25:53 +0200

    Initial Commit

---
+++ TabBarDemo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabBarDemo
+//
+//  Created by Martin Løseth Jensen on 30/04/2019.
+//  Copyright © 2019 Martin Løseth Jensen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabBarDemo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabBarDemo
+//
+//  Created by Martin Løseth Jensen on 30/04/2019.
+//  Copyright © 2019 Martin Løseth Jensen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
